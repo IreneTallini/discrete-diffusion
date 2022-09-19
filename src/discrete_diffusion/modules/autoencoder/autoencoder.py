59c07d2425f9dc2b078134f3854d8fada29c3235
EmilianPostolache <emilianpos@gmail.com>
2022-09-19 11:42:04 +0200

    Variational training

--- src/discrete_diffusion/modules/autoencoder/autoencoder.py
+++ src/discrete_diffusion/modules/autoencoder/autoencoder.py
@@ -32,9 +32,8 @@ class AttnBlock(nn.Module):
 
 
 def log_normal_pdf(sample, mean, logvar, dim=1):
-    log2pi = torch.log(2. * np.pi)
-    return torch.sum(
-        -.5 * ((sample - mean) ** 2. * torch.exp(-logvar) + logvar + log2pi), dim=dim)
+    log2pi = torch.log(2. * torch.tensor(np.pi).type_as(sample))  # .as_type(sample)
+    return torch.sum(-.5 * ((sample - mean) ** 2. * torch.exp(-logvar) + logvar + log2pi))
 
 
 class Autoencoder(torch.nn.Module):
@@ -53,7 +52,7 @@ class Autoencoder(torch.nn.Module):
             nn.SiLU(),
             AttnBlock(128),
             nn.SiLU(),
-            nn.Linear(128, 20))
+            nn.Linear(128, 256))
 
         self.decoder = torch.nn.Sequential(
             nn.Linear(128, 10))
@@ -61,30 +60,31 @@ class Autoencoder(torch.nn.Module):
     def encode(self, batch):
         edge_index = batch.edge_index
         adj_matrix = edge_index_to_adj(edge_index, num_nodes=10).to(torch.float32)
-        mean, logvar = torch.split(self.encoder(adj_matrix), split_size_or_sections=batch.batch.shape[0], dim=1)
+        emb = self.encoder(adj_matrix)
+        mean, logvar = torch.split(self.encoder(adj_matrix), split_size_or_sections=emb.shape[-1] // 2, dim=1)
         return mean, logvar
 
     def decode(self, z):
         return self.decoder(z)
 
     def reparameterize(self, mean, logvar):
-        eps = torch.randn(size=mean.shape)
+        eps = torch.randn(size=mean.shape).type_as(mean)
         return eps * torch.exp(logvar * .5) + mean
 
     def forward(self, batch):
-        # x = torch.rand((10, 10)).type_as(batch.edge_index).to(torch.float32)
+        # x = torch.rand((10, 10)).t
+        # ype_as(batch.edge_index).to(torch.float32)
         # x.requires_grad = True
         mean, logvar = self.encode(batch)
-        return mean
-
-        # mean, logvar = self.encode(batch)
-        # z = self.reparameterize(mean, logvar)
-        # x = torch.decode(z)
+        z = self.reparameterize(mean, logvar)
+        x = self.decode(z)
         # # cross_ent = torch.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
         # # logpx_z = -torch.reduce_sum(cross_ent, axis=[1, 2, 3])
-        # logpz = log_normal_pdf(z, 0., 0.)
-        # logqz_x = log_normal_pdf(z, mean, logvar)
-        # return -torch.mean(logpx_z + logpz - logqz_x)
-        # return x
+
+        gt_adjs = edge_index_to_adj(batch.edge_index, len(batch.batch))
+        logpx_z = -torch.norm(gt_adjs - x) ** 2
+        logpz = log_normal_pdf(z, torch.tensor(0.).type_as(z), torch.tensor(0.).type_as(z))
+        logqz_x = log_normal_pdf(z, mean, logvar)
+        return -torch.mean(logpx_z + logpz - logqz_x), z, x
 
 #%%
--- src/discrete_diffusion/pl_modules/autoencoder_pl_module.py
+++ src/discrete_diffusion/pl_modules/autoencoder_pl_module.py
@@ -24,33 +24,35 @@ pylogger = logging.getLogger(__name__)
 class AutoencoderPLModule(TemplatePLModule):
 
     def step(self, batch) -> Mapping[str, Any]:
-        similarities = self(batch)
+        loss, z, x_rec = self(batch)
+        # edge_index = batch.edge_index
+        # adj_matrix = edge_index_to_adj(edge_index, num_nodes=10).to(torch.float32)  # .to(self.device)
+
         # graph_sizes = get_graph_sizes_from_batch(batch)
         # mask = torch.block_diag(*[torch.triu(torch.ones(i, i), diagonal=1) for i in graph_sizes]).bool()
-        gt_adjs = edge_index_to_adj(batch.edge_index, len(batch.batch))
 
         # loss = torch.norm(gt_adjs[mask] - similarities[mask]) ** 2
         # loss = torch.sum(torch.abs(gt_adjs - similarities))
-        loss = torch.norm(gt_adjs - similarities) ** 2
         # loss = torch.binary_cross_entropy_with_logits(similarities[mask], gt_adjs[mask].float()).mean()
-        return {"loss": loss, "similarities": similarities}
+        return {"loss": loss, "z": z, "x_rec": x_rec}
 
     def validation_step(self, batch: Any, batch_idx: int) -> Mapping[str, Any]:
         logger: NNLogger
         step_out = self.step(batch)
-        similarities = step_out['similarities']
+        z = step_out['z']
+        x_rec = step_out['x_rec']
         # z = step_out["z"]
         # similarities = z @ z.T
         if batch_idx < 5:
             fig, axs = plt.subplots(2, 2, constrained_layout=True)
             gt_adjs = edge_index_to_adj(batch.edge_index, len(batch.batch))
-            im = axs[0, 0].imshow(similarities.T.cpu(), cmap='coolwarm')
+            im = axs[0, 0].imshow(x_rec.T.cpu(), cmap='coolwarm')
             axs[0, 0].set_title("reconstruction")
             plt.colorbar(im, ax=axs[0, 0], orientation='vertical')
             axs[0, 1].imshow(gt_adjs.T.cpu())
             axs[0, 1].set_title("ground truth")
 
-            disc_adj = (similarities > 0.5).long()
+            disc_adj = (x_rec > 0.5).long()
             edge_index = adj_to_edge_index(disc_adj)
             data = get_data_from_edge_index(edge_index, batch.x)
             nx.draw(torch_geometric.utils.to_networkx(data), with_labels=True, ax=axs[1, 0], node_size=0.1)
